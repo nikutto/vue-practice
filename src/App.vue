ccf52c2dbbea3c79138303df8873d9650b744aad
Jumpei Mori <nikutto.kom@gmail.com>
2022-09-23 17:08:45 +0900

    Create Hello world with vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <HelloWorld msg="Hello world!" />
+  </main>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      This is example of vue page.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1 {
+  text-align: center;
+}
+.greetings h3 {
+  text-align: center;
+}
+
+</style>
